1ccf46182a0e3312f922afa92e730b979c1a4437
stevesklo <smsklo@udel.edu>
2015-03-17 12:18:55 -0400

    initial

---
+++ src/main/desktop/src/edu/udel/cisc275_15S/advisementadventure/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package edu.udel.cisc275_15S.advisementadventure.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import edu.udel.cisc275_15S.advisementadventure.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
