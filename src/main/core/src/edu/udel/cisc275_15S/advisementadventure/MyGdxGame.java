7cd171cdc07af77c6e6cef3af71367d41c84af44
catherinemarymorse <catherinemarymorse@yahoo.com>
2015-03-26 01:47:49 -0400

    implemented initial layout and flow for prototype

--- src/main/core/src/edu/udel/cisc275_15S/advisementadventure/MyGdxGame.java
+++ src/main/core/src/edu/udel/cisc275_15S/advisementadventure/MyGdxGame.java
@@ -1,27 +1,85 @@
 package edu.udel.cisc275_15S.advisementadventure;
 
+import java.util.Stack;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	Screen currentScreen;
+	Stack<Screen> stack;
+
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create() {
+		currentScreen = Screen.HOME;
+		stack = new Stack<Screen>();
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	public void render() {
+		if (Gdx.input.justTouched()) {
+			if (currentScreen != Screen.HOME) {
+				otherScreenClick();
+			} else {
+				homeScreenClick();
+			}
+		}
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		currentScreen.changeScreen();
 	}
+
+	public void homeScreenClick() {
+		stack.add(currentScreen);
+		System.out.println("X: " + Gdx.input.getX() + ", Y: "+ Gdx.input.getY());
+		int clickX = Gdx.input.getX();
+		int clickY = Gdx.input.getY();
+		if (clickX >= 70 && clickX <= 150 && clickY >= 50 && clickY <= 180) {
+			currentScreen = Screen.NOTES;
+		} else if (clickX >= 170 && clickX <= 250 && clickY >= 50
+				&& clickY <= 180) {
+			currentScreen = Screen.WEB;
+		} else if (clickX >= 270 && clickX <= 350 && clickY >= 50
+				&& clickY <= 180) {
+			currentScreen = Screen.TEXT1;
+		} else if (clickX >= 370 && clickX <= 450 && clickY >= 50
+				&& clickY <= 180) {
+			currentScreen = Screen.EMAIL1;
+		} else if (clickX >= 470 && clickX <= 550 && clickY >= 50
+				&& clickY <= 180) {
+			currentScreen = Screen.HELP;
+		}
+
+	}
+
+	public void otherScreenClick() {
+		System.out.println("X: " + Gdx.input.getX() + ", Y: "+ Gdx.input.getY());
+		int clickX = Gdx.input.getX();
+		int clickY = Gdx.input.getY();
+		if (clickX >= 0 && clickX <= 100 && clickY >= 0 && clickY <= 100) {
+			currentScreen = stack.pop();
+		}
+
+	}
+
+	// original badlogic default gdx game implementation
+
+	// SpriteBatch batch;
+	// Texture img;
+	//
+	// @Override
+	// public void create() {
+	// batch = new SpriteBatch();
+	// img = new Texture("badlogic.jpg");
+	// }
+	//
+	// @Override
+	// public void render() {
+	// Gdx.gl.glClearColor(1, 0, 0, 1);
+	// Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	// batch.begin();
+	// batch.draw(img, 0, 0);
+	// batch.end();
+	// }
 }
---
+++ src/main/core/src/edu/udel/cisc275_15S/advisementadventure/Screen.java
@@ -0,0 +1,93 @@
+package edu.udel.cisc275_15S.advisementadventure;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public enum Screen {
+	HOME,
+	EMAIL1,
+	EMAIL2,
+	WEB,
+	UDSIS,
+	RSVP,
+	TEXT1,
+	TEXT2,
+	NOTES,
+	HELP;
+	
+	private SpriteBatch batch = new SpriteBatch();
+	
+	public SpriteBatch getBatch() {
+		return batch;
+	}
+	
+	public void setBatch(SpriteBatch batch) {
+		this.batch = batch;
+	}
+	
+	public void changeScreen() {
+
+		batch.begin();
+		
+		BitmapFont font = new BitmapFont();
+		font.setColor(0, 0, 0, 1);
+		
+		Texture btnBack = new Texture("btn_back.png");
+		batch.draw(btnBack, 25, 425);
+		
+		Texture btnNotes = new Texture("btn_notes.png");
+		Texture btnWeb = new Texture("btn_web.png");
+		Texture btnText = new Texture("btn_text.png");
+		Texture btnEmail = new Texture("btn_email.png");
+		Texture btnHelp = new Texture("btn_help.png");
+		
+		switch (this) {
+		case EMAIL1:
+			font.draw(batch, "Email - Screen 1", 300, 250);
+			break;
+		case EMAIL2:
+			font.draw(batch, "Email - Screen 2", 300, 250);
+			break;
+		case HELP:
+			font.draw(batch, "Help", 300, 250);
+			break;
+		case HOME:
+			batch.draw(btnNotes, 75, 350);
+			batch.draw(btnWeb, 175, 350);
+			batch.draw(btnText, 275, 350);
+			batch.draw(btnEmail, 375, 350);
+			batch.draw(btnHelp, 475, 350);
+			
+			font.draw(batch, "Notes", 95, 325);
+			font.draw(batch, "Web", 195, 325);
+			font.draw(batch, "Texts", 295, 325);
+			font.draw(batch, "Email", 395, 325);
+			font.draw(batch, "Help", 495, 325);
+			break;
+		case NOTES:
+			font.draw(batch, "Notes", 300, 250);
+			break;
+		case RSVP:
+			font.draw(batch, "Web - RSVP", 300, 250);
+			break;
+		case TEXT1:
+			font.draw(batch, "Text - Screen 1", 300, 250);
+			break;
+		case TEXT2:
+			font.draw(batch, "Text - Screen 2", 300, 250);
+			break;
+		case UDSIS:
+			font.draw(batch, "Web - UDSIS", 300, 250);
+			break;
+		case WEB:
+			font.draw(batch, "Web", 300, 250);
+			break;
+		default:
+			break;
+		
+		}
+
+		batch.end();
+	}
+}
