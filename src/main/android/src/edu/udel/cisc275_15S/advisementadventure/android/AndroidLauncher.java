5569d7bcda8a3fc93e537138fd44f12281a0ca23
catherinemarymorse <catherinemarymorse@yahoo.com>
2015-04-05 11:57:20 -0400

    optimized imports to remove warnings

--- src/main/android/src/edu/udel/cisc275_15S/advisementadventure/android/AndroidLauncher.java
+++ src/main/android/src/edu/udel/cisc275_15S/advisementadventure/android/AndroidLauncher.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
 import edu.udel.cisc275_15S.advisementadventure.MyGdxGame;
 
 public class AndroidLauncher extends AndroidApplication {
