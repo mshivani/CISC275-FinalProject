0f5ef4df724a3bf933d0d8b04434b6b3ef9830f4
catherinemarymorse <catherinemarymorse@yahoo.com>
2015-04-05 12:05:06 -0400

    test

--- src/main/core/src/edu/udel/cisc275_15S/advisementadventure/Email2Screen.java
+++ src/main/core/src/edu/udel/cisc275_15S/advisementadventure/Email2Screen.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+// liuh
 public class Email2Screen extends ScreenAdapter {
 
 	MyGdxGame game;
