06e42e0fefb9709569810200a6093ce4322bac36
jeremyake <ake@udel.edu>
2015-04-07 14:38:01 -0400

    test by Jeremy

--- src/main/desktop/src/edu/udel/cisc275_15S/advisementadventure/desktop/DesktopLauncher.java
+++ src/main/desktop/src/edu/udel/cisc275_15S/advisementadventure/desktop/DesktopLauncher.java
@@ -3,7 +3,7 @@ package edu.udel.cisc275_15S.advisementadventure.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import edu.udel.cisc275_15S.advisementadventure.MyGdxGame;
-
+//test by Jeremy
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
